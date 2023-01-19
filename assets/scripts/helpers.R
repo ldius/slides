09625a9ce3d89219511cea104e1591855849ff74
Joseph V. Casillas <joseph.casillas@rutgers.edu>
2023-01-19 12:00:18 -0500

    initial commit

---
+++ assets/scripts/helpers.R
@@ -0,0 +1,41 @@
+# Packages --------------------------------------------------------------------
+
+# Custom
+library("ds4ling")
+library("lingStuff")
+library("untidydata")
+
+# Tidyverse
+library("dplyr")
+library("tidyr")
+library("readr")
+library("purrr")
+library("here")
+library("forcats")
+
+# Modeling
+library("lme4")
+library("brms")
+
+# Plotting
+library("ggplot2")
+library("patchwork")
+library("ggridges")
+library("ggrepel")
+library("viridis")
+library("ggfortify")
+
+# Misc
+library("knitr")
+library("kableExtra")
+library("latex2exp")
+library("broom")
+library("fGarch")
+library("RefManageR")
+library("equatiomatic")
+
+# xaringan stuff
+library("tiktokrmd")
+
+# set seed for reproducibility
+set.seed(12345)
