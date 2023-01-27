2deefcee12009b8d6b98da718c0765bf527ca49e
Joseph V. Casillas <joseph.casillas@rutgers.edu>
2023-01-26 21:49:15 -0500

    test qr

--- assets/scripts/helpers.R
+++ assets/scripts/helpers.R
@@ -1,10 +1,5 @@
 # Packages --------------------------------------------------------------------
 
-# Custom
-library("ds4ling")
-library("lingStuff")
-library("untidydata")
-
 # Tidyverse
 library("dplyr")
 library("tidyr")
@@ -13,26 +8,14 @@ library("purrr")
 library("here")
 library("forcats")
 
-# Modeling
-library("lme4")
-library("brms")
-
 # Plotting
 library("ggplot2")
 library("patchwork")
-library("ggridges")
-library("ggrepel")
-library("viridis")
-library("ggfortify")
 
 # Misc
 library("knitr")
 library("kableExtra")
-library("latex2exp")
-library("broom")
-library("fGarch")
 library("RefManageR")
-library("equatiomatic")
 
 # xaringan stuff
 library("tiktokrmd")
