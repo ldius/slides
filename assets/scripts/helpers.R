0e5722be11f33de7d16188dc16a148d7a1ff79d9
Joseph V. Casillas <joseph.casillas@rutgers.edu>
2023-02-17 10:05:05 -0500

    wek 5 slides

--- assets/scripts/helpers.R
+++ assets/scripts/helpers.R
@@ -7,9 +7,12 @@ library("readr")
 library("purrr")
 library("here")
 library("forcats")
+library("fs")
+library("stringr")
 
 # Plotting
 library("ggplot2")
+library("ggdark")
 library("patchwork")
 
 # Misc
@@ -20,5 +23,16 @@ library("RefManageR")
 # xaringan stuff
 library("tiktokrmd")
 
+# -----------------------------------------------------------------------------
+
 # set seed for reproducibility
 set.seed(12345)
+
+# Download bibfile 
+url <- "https://www.jvcasillas.com/syllabi/293/refs/bib/ldius.bib"
+
+download.file(
+  url, 
+  destfile = here("assets", "bib", "ldius.bib"), 
+  method = "curl"
+  )
